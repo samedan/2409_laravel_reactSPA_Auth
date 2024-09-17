84021aff3f8595b5f0785d228f01d3b2f863b998
Daniel Popescu <same.dan@gmail.com>
2024-09-17 16:00:52 +0200

    CRUD finished without Auth

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
+    /** GET /api/posts
      * Display a listing of the resource.
      */
     public function index()
@@ -17,40 +17,49 @@ class PostController extends Controller
         return Post::all();
     }
 
-    /**
+    /** POST /api/posts/
      * Store a newly created resource in storage.
      */
     // public function store(StorePostRequest $request)
     public function store(Request $request)
     {
-        $request->validate([
+        $fields = $request->validate([
             'title' => 'required|max:255',
             'body' => 'required',
         ]);
-        return 'Store ok';
+
+        $post = Post::create($fields);
+        return ['post' => $post];
+        
     }
 
-    /**
+    /** GET /api/posts/{post}
      * Display the specified resource.
      */
     public function show(Post $post)
     {
-        //
+        return ['post' => $post];
     }
 
-    /**
+    /** PUT /api/posts/{post}
      * Update the specified resource in storage.
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $fields = $request->validate([
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ]);
+        $post->update($fields);
+        return $post;
     }
 
-    /**
+    /** DELETE /api/posts/{post}
      * Remove the specified resource from storage.
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return ['message' => "Post deleted"];
     }
 }
