8247ea8436f9654e8f2b7d6f8c16fd83d1dfc3af
Daniel Popescu <same.dan@gmail.com>
2024-09-19 10:43:08 +0200

    Auth Ok, Backend & Frontend

--- react-frontend/src/Context/AppContext.jsx
+++ react-frontend/src/Context/AppContext.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 export default function AppProvider({ children }) {
     const [token, setToken] = useState(localStorage.getItem("token"));
     const [user, setUser] = useState(null);
@@ -28,6 +29,7 @@ export default function AppProvider({ children }) {
         if (token) {
             getUser();
         }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [token]);
 
     return (
